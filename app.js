202410101e022b3c6187a93f646567e8c41c57b0
Pauliina Solanne <pauliina.solanne@hoxhunt.com>
2020-10-27 09:31:18 +0200

    allow cors

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ app.use("/api/cards", cardRouter);
 
 app.use(cors());
 
-app.options("*", cors());
+res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+res.setHeader(
+  "Access-Control-Allow-Methods",
+  "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+);
+res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+res.setHeader("Access-Control-Allow-Credentials", true);
 
 app.get("/", (req, res) => {
   // res.send(cards);
